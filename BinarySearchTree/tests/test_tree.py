9d0d7f5e6317faf2decfc668c18c02b53deeb3d3
roryjgarland <rjg123456@hotmail.com>
2021-03-20 22:26:43 +0000

    Updating unit tests

--- BinarySearchTree/tests/test_tree.py
+++ BinarySearchTree/tests/test_tree.py
@@ -1,10 +1,18 @@
 import unittest
+
 from BinarySearchTree.tree import BST, Node
+
 
 class TreeTest(unittest.TestCase):
 
     def setUp(self) -> None:
         self.tree = BST()
+
+    def test_insert_method(self) -> None:
+        self.tree.insert(14)
+
+        self.assertIsInstance(self.tree.root, Node)
+        self.assertEqual(14, self.tree.root.value)
 
     def test_find_method(self) -> None:
         self.tree.insert(14)
@@ -13,5 +21,8 @@ class TreeTest(unittest.TestCase):
         self.tree.insert(9)
         self.tree.insert(32)
 
-        self.assertIsInstance(self.tree.find(23), Node)
+        value_in_tree = self.tree.find(23)
+        self.assertIsInstance(value_in_tree, Node)
+        self.assertEqual(value_in_tree.value, 23)
+
         self.assertIsNone(self.tree.find(43))
